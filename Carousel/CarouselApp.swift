680cebb1b538844294a3b029f7e3ca0507a540d0
Samuel Ailemen <samuel@verified.ly>
2022-04-01 22:32:54 -0400

    Initial Commit

---
+++ Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Samuel Ailemen on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
